688257f21c60950f938c2cc2adf500798ca6d212
sara <sara.antunes.franco@tecnico.ulisboa.pt>
2018-11-21 22:53:45 +0000

    Added style.

--- line.js
+++ line.js
@@ -1,5 +1,5 @@
 var svg = d3.select("#linesvg"),
-          margin = {top: 20, right: 20, bottom: 30, left: 40},
+          margin = {top: 20, right: 30, bottom: 30, left: 60},
           width = +svg.attr("width") - margin.left - margin.right,
           height = +svg.attr("height") - margin.top - margin.bottom;
 
@@ -30,14 +30,19 @@ var svg = d3.select("#linesvg"),
           g.append("g")
               .attr("class", "axis axis--x")
               .attr("transform", "translate(0," + height + ")")
-              .call(d3.axisBottom(x));
+              .call(d3.axisBottom(x))
+              .append("text")
+                .attr("class", "axis-title")
+                .attr("transform", "translate(" + width + "," + 30 + ")")
+                .text("Year");
 
           g.append("g")
               .attr("class", "axis axis--y")
               .call(d3.axisLeft(y).ticks(6).tickFormat(function(d) { return parseInt(d / 1000) + "k"; }))
             .append("text")
               .attr("class", "axis-title")
-              .attr("transform", "rotate(-90)")
+              .attr("transform", "rotate(0)")
+              .attr("transform", "translate(" + 0 + "," + -20 + ")")
               .attr("y", 6)
               .attr("dy", ".71em")
               .style("text-anchor", "end")
