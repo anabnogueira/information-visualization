6614f9ed7acbf0707b011f5cf54ca5169e3543e5
sara <sara.antunes.franco@tecnico.ulisboa.pt>
2018-11-22 21:08:13 +0000

    Add countries from map to selected countries.

--- map.js
+++ map.js
@@ -84,7 +84,14 @@ function ready(error, data, population) {
         tip.show(d);
         d3.select(this)
           .style('opacity', 1)
-          .style('stroke-width', 3);
+          .style('stroke-width', 3)
+          .html(d => d)
+          .on("click", function(d){
+            console.log(d.properties.name);
+            var sc = document.getElementById("selectedCountry")
+            var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+            sc.innerHTML += "<li>"+ d.properties.name + "<span class=" + "close" + ">" + "&times;"+ "</span>" + "<span class=" + "coloring" + " style=\"background-color:" + hue + ";\">" + "</span>"+ "</li>";  
+          });
       })
       .on('mouseout', function(d){
         tip.hide(d);
@@ -99,151 +106,9 @@ function ready(error, data, population) {
     .attr('d', path);
 }
 
-
-
-/*const format = d3.format(',');
-
-// Set tooltips
-const tip = d3.tip()
-  .attr('class', 'd3-tip')
-  .offset([-10, 0])
-  .html(d => `<strong>Country: </strong><span class='details'>${d.properties.name}<br></span><strong>Mortality Rate: </strong><span class='details'>${format(d.rate)}</span>`);
-
-const margin_map = {top: 0, right: 0, bottom: 0, left: 0};
-const width_map = 500 - margin_map.left - margin_map.right;
-const height_map = 300 - margin_map.top - margin_map.bottom;
-
-const color = d3.scaleThreshold()
-  .domain([
-    10000,
-    100000,
-    500000,
-    1000000,
-    5000000,
-    10000000,
-    50000000,
-    100000000,
-    500000000,
-    1500000000
-  ])
-  .range([
-    'rgb(247,251,255)',
-    'rgb(222,235,247)', 
-    'rgb(198,219,239)', 
-    'rgb(158,202,225)',
-    'rgb(107,174,214)',
-    'rgb(66,146,198)',
-    'rgb(33,113,181)',
-    'rgb(8,81,156)',
-    'rgb(8,48,107)',
-    'rgb(3,19,43)'
-  ]);
-
-const svg_map = d3.select('#worldmap')
-  .append('svg')
-  .attr('width', width_map)
-  .attr('height', height_map)
-  .append('g')
-  .attr('class', 'map');
-
-const projection = d3.geoRobinson()
-  .scale(148)
-  .rotate([352, 0, 0])
-  .translate( [width_map / 2, height_map / 2]);
-
-const path = d3.geoPath().projection(projection);
-
-svg_map.call(tip);
-
-queue()
-  .defer(d3.json, 'world_countries.json')
-  .defer(d3.tsv, 'world_population.tsv')
-  .await(ready); */
-
-/*var mortality_sorted;
-
-d3.csv("datasets/mortality_rate_sorted_cc.csv")
-    .row(function(d) {
-      return {
-        name: d.name,
-        rate: d.rate,
-        code: d.code,
-        year: d.year_span
-      };
-    })
-    .get(function(error, rows) {
-      console.log(rows);
-      mortality_sorted = rows;
-      ready(mortality_sorted);
-    });
-      */
 /*
-var mortality_sorted;
-
-d3.json("datasets/mortality_rate_sorted_cc.json")
-.row(function(d) {
-  return {
-    name: d.name,
-    rate: d.rate,
-    code: d.code,
-    year: d.year_span
-  };
-  })
-  .get(function(error, rows) {
-  console.log(rows);
-  mortality_sorted = rows;
-  ready(mortality_sorted);
-});
-
-*/
-
-/*var mort;
-
-d3.json("datasets/mortality_rate_sorted_cc.json").then(function(data) {
-    mort = data;
-    ready(mort);
-	
-});
-
-
-function ready(data) {
-  const rateByCode = {};
- var y = "1990 - 1995";
- /*  var code = "MAR";
-  for (i=0 ; i < data.length; i++){
-    if
-  } 
-  data[y].forEach(d => { rateByCode[data[y].code] = data[y].rate; });
-  data.features.forEach(d => { data.rate = rateByCode[data.code] });
-
-  svg.append('g')
-    .attr('class', 'countries')
-    .selectAll('path')
-    .data(data.features)
-    .enter().append('path')
-      .attr('d', path)
-      .style('fill', data => color(rateByCode[data.code]))
-      .style('stroke', 'white')
-      .style('opacity', 0.8)
-      .style('stroke-width', 0.3)
-      // tooltips
-      .on('mouseover',function(data){
-        tip.show(data);
-        d3.select(this)
-          .style('opacity', 1)
-          .style('stroke-width', 3);
-      })
-      .on('mouseout', function(d){
-        tip.hide(data);
-        d3.select(this)
-          .style('opacity', 0.8)
-          .style('stroke-width',0.3);
-      });
-
-  svg.append('path')
-    .datum(topojson.mesh(data.features, (a, b) => a.code !== b.code))
-    .attr('class', 'names')
-    .attr('d', path);
-}
-
-*/
+d3.select("worldmap")
+  .html(d => `<strong>Country: </strong><span class='details'>${d.properties.name}<br></span><strong>Mortality Rate: </strong><span class='details'>${format(d.population)}</span>`)
+  .on("click", function(d){
+    console.log(d.properties.name);
+  }); */
