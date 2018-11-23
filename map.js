fe49ee268075468b150eeaebf18b71714b56d9d5
Ana Nogueira <ana.nogueira.mu@gmail.com>
2018-11-23 17:53:02 +0000

    Functional slide.

--- map.js
+++ map.js
@@ -59,10 +59,95 @@ const path = d3.geoPath().projection(projection);
 
 svg_map.call(tip);
 
+
+
+
+
+
+// *****************  SLIDER TO SELECT CURRENT YEAR *************
+
+current_year = 1990;
+
+
+var filename_template = "mortality_rate_sorted";
+var filename = "";
+
 queue()
   .defer(d3.json, 'world_countries.json')
-  .defer(d3.tsv, 'mortality_rate_sorted1990.tsv')
+  .defer(d3.tsv, filename_template + "1990.tsv")
   .await(ready);
+
+
+
+$( function() {
+  $( "#slider-range-max" ).slider({
+    range: "max",
+    min: 1990,
+    max: 2015,
+    value: 1990,
+    slide: function( event, ui ) {
+      $( "#current_year" ).val( ui.value);
+      current_year = $( "#slider-range-max" ).slider( "value" ) ;
+
+      
+
+console.log(current_year);
+
+if (1990 <= current_year && current_year <= 1994) {
+  filename = filename_template + "1990.tsv";
+  console.log(filename);
+}
+
+if (1995 <= current_year && current_year <= 1999) {
+  filename = filename_template + "1995.tsv";
+  console.log(filename);
+}
+
+if (2000 <= current_year && current_year <= 2004) {
+  filename = filename_template + "2000.tsv";
+  console.log(filename);
+}
+
+if (2005 <= current_year && current_year <= 2009) {
+  filename = filename_template + "2005.tsv";
+  console.log(filename);
+}
+
+if (2010 <= current_year && current_year <= 2015) {
+  filename = filename_template + "2010.tsv";
+  console.log(filename);
+}
+
+queue()
+  .defer(d3.json, 'world_countries.json')
+  .defer(d3.tsv, filename)
+  .await(ready);
+
+
+
+    }
+
+  });
+
+} );
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function ready(error, data, population) {
   const populationById = {};
