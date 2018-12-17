1b288f9d2d9c471817c44846537db9f79534a98a
sara <sara.antunes.franco@tecnico.ulisboa.pt>
2018-12-17 12:42:19 +0000

    Gooey changing with factors.

--- gooeyalcohol.js
+++ gooeyalcohol.js
@@ -1,16 +1,8 @@
 var DATA_FILE_LOC = '/datasets/factors/sanitation.tsv';
 var USER_YEAR = '2015'; // default user year
 
-// var cells = [[10,10], [93,10], [176,10], [259,10], [342,10],
-//          [10,93], [93,93], [176,93], [259,93], [342,93],
-//          [10,176], [93,176], [176,176], [259,176], [342,176],
-//          [10,259], [93,259], [176,259], [259,259], [342,259]]
 
-var cells_orig = [[407,152], [322,152], [237,322], [237,67], [67,322], [415,67], [407,322], [67,67],
-            [67,152], [237,237], [152,67], [407,237], [152,322], [152,237], [322,237], [322,67],
-              [322,322], [67,237], [152,152], [237,152]]
-
-var cells = cells_orig;
+var constGooey=20;
 
 var margin = {top: 10, right: 10, bottom: 10, left: 10},
   width = 700 - margin.left - margin.right,
@@ -29,20 +21,12 @@ var g = svgGooey.append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-
-///////////////////////////////////////////////////////////////////////////
-///////////////////////////// Create filter ///////////////////////////////
-/////////////////////////////////////////////////////////////////////////// 
-
-//SVG filter for the gooey effect
-//Code taken from http://tympanus.net/codrops/2015/03/10/creative-gooey-effects/
-
 var defs = svgGooey.append("defs");
 var filter = defs.append("filter").attr("id","gooeyCodeFilter");
 filter.append("feGaussianBlur")
   .attr("in","SourceGraphic")
   .attr("stdDeviation","10")
-  //to fix safari: http://stackoverflow.com/questions/24295043/svg-gaussian-blur-in-safari-unexpectedly-lightens-image
+
   .attr("color-interpolation-filters","sRGB") 
   .attr("result","blur");
 
@@ -52,16 +36,10 @@ filter.append("feColorMatrix")
   .attr("values","1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7")
   .attr("result","gooey");
 
-// Foci
 var color1 = "#ff8000";
 var foci = {
     "dragger": { x: 0, y: 0, color: color1 }
 };
-
-// SAMPLE COUNTRIES
-var s_countries = ["Antigua and Barbuda","Denmark", "China", "Russia", "Spain", "Portugal", "France",  "Ghana", "Sweden", "Germany",
-  "Comoros"]
-// USA does not work - probably because there are two things with USA on the TSV, US and US virgin islands
 
 var root;
 
@@ -124,6 +102,14 @@ function gooey_switch(args){
   const { factorname } = args;
   console.log(args);
   DATA_FILE_LOC = "/datasets/factors/" +args + ".tsv";
+
+  if(args=="sanitation"){constGooey=20;}
+  if(args=="calories"){constGooey=0.002;}
+  if(args=="daily_smoking"){constGooey=0.5;}
+  if(args=="vaccination"){constGooey=10000;}
+  if(args=="gdp"){constGooey=0.00000000002;}
+  //if(args=="hdi"){constGooey=10000;}
+  if(args=="co2_emissions"){constGooey=0.001;}
   d3v3.tsv(DATA_FILE_LOC, type, function(error, info) {
    console.log(DATA_FILE_LOC);
     gooeyData = [];
@@ -134,7 +120,6 @@ function gooey_switch(args){
 }
 
 const svgLabels = svgGooey.append("g").attr("class", "labels-container");
-//var nodes= [{ radius: 0, fixed: true, choice: "dragger", idd: "root" }];
 
 
 function drawGooey(countriesSelected) {
@@ -142,7 +127,6 @@ function drawGooey(countriesSelected) {
         year_data[d.year] = d;
   });
 
-  // Create node objects
   var nodes = [{ radius: 0, fixed: true, choice: "dragger", idd: "root" }];
   root = nodes[0];
 
@@ -158,25 +142,17 @@ function drawGooey(countriesSelected) {
         
       if (foci[str] == null) {
         foci[str] = { 
-          // x : pos[0],
-          // y : pos[1],
           x : Math.floor(Math.random() * (550 - 50)) + 50,
-          y : Math.floor(Math.random() * (350 - 50)) + 50,
+          y : Math.floor(Math.random() * (300 - 50)) + 50,
           color: col
-          //color : 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')'
         };  
       }
-      // cells.splice(0, 1);
-      d3v3.range(0, 0.5 * year_data[USER_YEAR][str]).map(function(o, i) {
+
+      d3v3.range(0, constGooey * year_data[USER_YEAR][str]).map(function(o, i) {
 
         nodes_so_far += 1;
-        //console.log(str)
-
-
-
 
           nodes.push({
-            //id: "node" + String(nodes_so_far),
             x: foci[str].x + Math.random(),
             y: foci[str].y + Math.random(),
             radius: node_radius,
@@ -194,7 +170,6 @@ function drawGooey(countriesSelected) {
     .nodes(nodes)
     .size([width, height])
     .gravity(0)
-    //.charge(function(d, i) { return i ? 0 : -400; })
     .friction(.93)
     .on("tick", tick)
     .start();
@@ -203,17 +178,14 @@ function drawGooey(countriesSelected) {
 svgGooey.on("mousemove", function() {
   var p1 = d3v3.mouse(this);
 
-  //console.log( d3v3.event.pageX, d3v3.event.pageY ) // log the mouse x,y position
   
   root.px = p1[0];
   root.py = p1[1];
   force.resume();
 });
   
-  // Draw circle for each node.
   var circleData = circleWrapper.selectAll("circle")
     .data(nodes, function(d) {
-      //console.log(d.idd);
       return d.idd;
   });
 
@@ -225,7 +197,6 @@ svgGooey.on("mousemove", function() {
     .attr("class", "node")
     .style("fill", function(d) { return foci[d.choice].color; });    
 
-  // For smoother initial transition to settling spots.
   circle.transition()
     .duration(900)
     .delay(function(d,i) { return i * 5; })
@@ -235,7 +206,6 @@ svgGooey.on("mousemove", function() {
     });
 
     const dataCountries = countriesSelected;
-    console.log(dataCountries);
     const svgLabelsData = svgLabels.selectAll('text').data(dataCountries, function(d) {
       return d;
     });
@@ -248,7 +218,6 @@ svgGooey.on("mousemove", function() {
           })
           .attr("class", "counter")
           .attr("data-choice", function(d) {
-            console.log(d);
             return d;
           })
           .attr("x", function(d) {
@@ -259,137 +228,9 @@ svgGooey.on("mousemove", function() {
           })
           .style("font-weight", "lighter")
           .style("font-family", "sans serif")
-         // .attr("text-anchor", "middle")
           .text(function(d) {
             return year_data[USER_YEAR][d];
           })
-
-    
-
-    // // Labels etc
-    // d3v3.keys(foci).forEach(function(d) {
-    //   if (d != "dragger") {
-    //     svgGooey.data()
-
-    //     svgGooey.append("text")
-    //       .attr("id", "counter"+d)
-    //       .attr("class", "counter")
-    //       .attr("data-choice", d)
-    //       .attr("x", foci[d].x)
-    //       .attr("y", foci[d].y)
-    //       .style("font-weight", "lighter")
-    //       .style("font-family", "sans serif")
-    //      // .attr("text-anchor", "middle")
-    //       .text(year_data[USER_YEAR][d]);
-
-    //     svgGooey.on("mousemove", function() {
-    //       var p1 = d3v3.mouse(this);
-
-    //       //console.log( d3v3.event.pageX, d3v3.event.pageY ) // log the mouse x,y position
-          
-    //       root.px = p1[0];
-    //       root.py = p1[1];
-    //       force.resume();
-    //     });
-    //   }   
-    // });
-
-    // // Country dropdown menu
-    // d3v3.select("#dropdown_title").html(USER_YEAR);
-    // d3v3.select(".dropdown-menu").selectAll("li")
-    //     .data(gooeyData)
-    //   .enter().append("li").append("a")
-    //     .text(function(d) { return d.year; })
-    //     .on("click", function() {
-    //         var selText = d3v3.select(this).text();
-    //         d3v3.select("#dropdown_title").html(selText);
-            
-    //         USER_YEAR = selText;
-    //        // timer();
-    //     });
-
-        
-
-    // d3v3.select("#button").on("click", function(d) {
-    //     if (USER_YEAR == "usa") {
-    //         USER_YEAR = "Australia";
-    //     } else {
-    //         USER_YEAR = "Paraguay";
-    //     }
-    //     console.log(USER_YEAR);
-    //
-    //     timer();
-    // })
-        
-    // svgGooey.on("mousemove", function() {
-    //     var p1 = d3v3.mouse(this);
-        
-    //     //console.log( d3v3.event.pageX, d3v3.event.pageY ) // log the mouse x,y position
-        
-    //     root.px = p1[0];
-    //     root.py = p1[1];
-    //     force.resume();
-    //   });
-
-    // Run function periodically to make things move.
-
-    var timeout;
-    function timer() {
-      nodes_so_far = 1;
-      d3v3.keys(year_data[USER_YEAR]).forEach(function(c) {
-        if (year_data[USER_YEAR][c] > 0) {
-          d3v3.range(0, year_data[USER_YEAR][c]).map(function(o, i) {
-            if (nodes_so_far <= 100) {
-              nodes[nodes_so_far].choice = c;
-              nodes_so_far += 1;
-            }
-          });
-        }
-      });
-    
-      force.resume();
-  
-      d3v3.selectAll("text.counter")
-        .transition()
-        .duration(600)
-        .tween("text", function(d) {
-          var re = /(\d+)%/;
-          var meta_array = re.exec(this.textContent);
-          if (meta_array) {
-            var just_number = meta_array[1];
-            if (just_number.substring(0,1) == "<") {
-              just_number = just_number.substring(1);
-            }
-          } 
-
-          else {
-            var just_number = 0;
-          }
-  
-          var choice = d3v3.select(this).attr("data-choice");
-          var new_pct = year_data[USER_YEAR][choice];
-          //console.log(choice);
-      
-          var i = d3v3.interpolate(just_number, new_pct);
-          return function(t) {
-            if (i(t) < 1 && i(t) > 0) {
-                this.textContent = "<1%";
-            }
-            else {
-              this.textContent = Math.round(i(t)) + "%";
-            }
-          }
-        });
-    
-        // Run it again in a few seconds.
-         timeout = setTimeout(timer, 400);
-    }
-
-
-
-    //
-    // Force-directed boiler plate functions
-    //
 
 
     function tick(e) {
@@ -403,7 +244,6 @@ svgGooey.on("mousemove", function() {
     }
 
 
-    // Move nodes toward cluster focus.
     function gravity(alpha) {
       return function(d) {
         d.y += (foci[d.choice].y - d.y) * alpha;
@@ -412,8 +252,6 @@ svgGooey.on("mousemove", function() {
     }
 
 
-
-    // Resolve collisions between nodes.
     function collide(alpha) {
       var quadtree = d3v3.geom.quadtree(nodes);
       return function(d) {
@@ -440,14 +278,7 @@ svgGooey.on("mousemove", function() {
         });
       };
     }
-   // cells = cells_orig;
 } // @end d3v3.tsv()
-
-
-
-
-
-
 
 
 function type(d, i) {
